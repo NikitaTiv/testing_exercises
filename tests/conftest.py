2cff6f65978f279699dd38db17703313d304a71f
NikitaTiv <118524352+NikitaTiv@users.noreply.github.com>
2023-02-25 05:58:04 +0300

    Test tasks review

---
+++ tests/conftest.py
@@ -0,0 +1,59 @@
+import pytest
+import datetime
+from functions.four_bank_parser import SmsMessage, Expense, BankCard
+from decimal import Decimal
+
+
+@pytest.fixture
+def long_string():
+    return ''.join('s' for n in range(100))
+
+
+@pytest.fixture
+def sms_text():
+    return '1020 руб, 4400430255123326 20.02.23 16:20 7/11 authcode 3034'
+
+
+@pytest.fixture
+def sms_message(sms_text):
+    return SmsMessage(
+        text=sms_text,
+        author="Sberbank",
+        sent_at=datetime.datetime(2023, 2, 20, 16, 20, 20),
+    )
+
+
+@pytest.fixture
+def bank_cards(bank_card):
+    return [
+        bank_card,
+        BankCard(last_digits='1815', owner='Olga N.'),
+        BankCard(last_digits='1855', owner='Roman G.'),
+    ]
+
+@pytest.fixture
+def bank_card():
+    return BankCard(
+        last_digits='3326',
+        owner='Nikita T.',
+    )
+
+
+@pytest.fixture
+def expense(bank_card):
+    return Expense(
+        amount=Decimal('1020'),
+        card=bank_card,
+        spent_in='7/11',
+        spent_at=datetime.datetime(2023, 2, 20, 16, 20),
+    )
+
+
+@pytest.fixture
+def today():
+    return datetime.date.today()
+
+
+@pytest.fixture
+def yesterday(today):
+    return today + datetime.timedelta(days=1)
--- tests/test_one_gender.py
+++ tests/test_one_gender.py
@@ -1,5 +1,13 @@
 from functions.one_gender import genderalize
+import pytest
 
 
-def test_genderalize():
-    pass
+@pytest.mark.parametrize(
+    'gender, expected_result',
+    [
+        ('male', 'male_verb'),
+        ('female', 'female_verb'),       
+    ]
+)
+def test_genderalize(gender, expected_result):
+    assert genderalize('male_verb', 'female_verb', gender) == expected_result
