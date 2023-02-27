5cca57ca994df36ec2b6bfb3f62d8a416db1d12e
NikitaTiv <118524352+NikitaTiv@users.noreply.github.com>
2023-02-27 04:18:24 +0300

    Delete level_1 folder

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,10 @@
-from functions.level_1.two_date_parser import compose_datetime_from
+import datetime
+from functions.two_date_parser import compose_datetime_from
 
 
-def test_compose_datetime_from():
-    pass
+def test_compose_datetime_from_today(today):
+    assert compose_datetime_from('today', '16 : 48') == datetime.datetime(today.year, today.month, today.day, 16, 48)
+
+
+def test_compose_datetime_from(yesterday):
+    assert compose_datetime_from('tomorrow', '16 : 48') == datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 16, 48)
